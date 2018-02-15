5a80d3efbe7cb3daae7746233b0a1794de059782
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2018-02-15 08:27:47 -0800

    initial card class and tests

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,3 +1,42 @@
 public class Card {
-  
+  private String suit;
+  private String faceValue;
+  private boolean faceUp;
+  private boolean inDeck;
+
+  public Card(String fv, String s) {
+    this.suit = s;
+    this.faceValue = fv;
+    this.faceUp = false;
+    this.inDeck = true;
+  }
+
+  public void showCard() {
+    this.faceUp = true;
+  }
+
+  public void hideCard() {
+    this.faceUp = false;
+  }
+
+  public void dealCard() {
+    this.inDeck = false;
+  }
+
+  public void returnCard() {
+    this.inDeck = true;
+  }
+
+  public boolean isInDeck() {
+    return this.inDeck;
+  }
+
+  @Override
+  public String toString() {
+    if(this.faceUp) {
+      return this.faceValue + this.suit;
+    } else {
+      return "XXX";
+    }
+  }
 }
--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,3 +1,37 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
 public class CardTest {
-  
+  private Card mCard;
+
+  @Before
+  public void initialize() {
+    mCard = new Card("A", "♠️");
+  }
+
+  @Test
+  public void cardToStringReturnsCorrectStringForFaceUp() {
+    mCard.showCard();
+    assertEquals("A♠️", mCard.toString());
+  }
+
+  @Test
+  public void cardToStringReturnsCorrectStringforNotFaceUp() {
+    mCard.hideCard();
+    assertEquals("XXX", mCard.toString());
+  }
+
+  @Test
+  public void dealtCardNotInDeck() {
+    mCard.dealCard();
+    assertEquals(false, mCard.isInDeck());
+  }
+
+  @Test
+  public void returnedCardInDeck() {
+    mCard.returnCard();
+    assertEquals(true, mCard.isInDeck());
+  }
+
+
 }
